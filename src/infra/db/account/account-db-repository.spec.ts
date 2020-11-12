1bbcc9703e20ae51e1d060c009590522ce51f961
Matheus Nunes <mathieusnunes@gmail.com>
2020-11-12 07:03:55 -0300

    test: add AccountDbRepository with loadByEmail and updateAccessToken

---
+++ src/infra/db/account/account-db-repository.spec.ts
@@ -0,0 +1,48 @@
+import { AccountDbRepository } from './account-db-repository'
+import { DbHelper } from '@/infra/db/helpers/db-helper'
+import { mockAddAccountModel } from '@/domain/test/mock-account'
+import Knex from 'knex'
+
+describe('Account Db Repository', () => {
+  let db: Knex
+
+  beforeAll(async (done) => {
+    db = await DbHelper.connect()
+    await db.migrate.latest()
+    done()
+  })
+
+  afterAll(async done => {
+    await db.destroy()
+    done()
+  })
+
+  beforeEach(async (done) => {
+    await db('accounts').delete()
+    done()
+  })
+
+  describe('loadByEmail()', () => {
+    test('Should return a account on loadByEmail success', async () => {
+      const sut = new AccountDbRepository()
+      await db('accounts').insert(mockAddAccountModel())
+      const account = await sut.loadByEmail('any_email@mail.com')
+      expect(account).toBeTruthy()
+    })
+    test('Should return a null if loadByEmail fails', async () => {
+      const sut = new AccountDbRepository()
+      const account = await sut.loadByEmail('any_email@mail.com')
+      expect(account).toBeNull()
+    })
+  })
+
+  describe('updateAccessToken()', () => {
+    test('Should update an access token on success', async () => {
+      const sut = new AccountDbRepository()
+      const result = await db('accounts').insert(mockAddAccountModel())
+      await sut.updateAccessToken(result[0], 'any_token')
+      const fakeAccount = await db('accounts').where({ id: result[0] }).select()
+      expect(fakeAccount[0].token).toEqual('any_token')
+    })
+  })
+})
