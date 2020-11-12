6fe426035def3cd5ecff1ac4704ba47f0b98fd66
Matheus Nunes <mathieusnunes@gmail.com>
2020-11-12 08:19:05 -0300

    feat: add JwtAdapter encrypt

---
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -0,0 +1,11 @@
+import { Encrypter } from '@/data/protocols/cryptography/encrypter'
+import jwt from 'jsonwebtoken'
+
+export class JwtAdapter implements Encrypter {
+  constructor (private readonly secret: string) {}
+
+  async encrypt (id: number): Promise<string> {
+    const accessToken = await jwt.sign({ id }, this.secret)
+    return accessToken
+  }
+}
