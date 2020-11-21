66eb71f3943f93bb21c79d9321537f0adf7185c1
Matheus Nunes <mathieusnunes@gmail.com>
2020-11-21 19:35:40 -0300

    test: add /signup route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -49,4 +49,17 @@ describe('Login Route', () => {
         .expect(401)
     })
   })
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Matheus',
+          email: 'mathieusnunes@gmail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
 })
