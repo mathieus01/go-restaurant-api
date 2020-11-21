c7952cae8d7873629e4582700cd8b1f65a0880ce
Matheus Nunes <mathieusnunes@gmail.com>
2020-11-21 11:02:52 -0300

    test: add login route

---
+++ src/main/routes/login-routes.test.ts
@@ -0,0 +1,52 @@
+import { DbHelper } from '@/infra/db/helpers/db-helper'
+import { hash } from 'bcrypt'
+import Knex from 'knex'
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Login Route', () => {
+  let db: Knex
+
+  beforeAll(async (done) => {
+    db = await DbHelper.connect()
+    await db.migrate.latest()
+    done()
+  })
+
+  afterAll(async done => {
+    await db.destroy()
+    done()
+  })
+
+  beforeEach(async (done) => {
+    await db('accounts').delete()
+    done()
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      const password = await hash('any_password', 12)
+      await db('accounts').insert({
+        name: 'any_name',
+        email: 'any_mail@mail.com',
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'any_mail@mail.com',
+          password: 'any_password'
+        })
+        .expect(200)
+    })
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'any_mail@mail.com',
+          password: 'any_password'
+        })
+        .expect(401)
+    })
+  })
+})
