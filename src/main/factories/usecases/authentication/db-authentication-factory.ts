65239ca2d7efb784af424eede7f49b643981e7a7
Matheus Nunes <mathieusnunes@gmail.com>
2020-11-21 11:01:12 -0300

    feat: add Login Factories

---
+++ src/main/factories/usecases/authentication/db-authentication-factory.ts
@@ -0,0 +1,13 @@
+import { DbAuthentication } from '@/data/usecases/account/authentication/db-authentication'
+import { Authentication } from '@/domain/usecases/account/authentication'
+import { BcryptAdapter } from '@/infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '@/infra/criptography/jwt-adapter/jwt-adapter'
+import { AccountDbRepository } from '@/infra/db/account/account-db-repository'
+import env from '@/main/config/env'
+
+export const makeDbAuthentication = (): Authentication => {
+  const bcryptAdapter = new BcryptAdapter()
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  const accountDbRepository = new AccountDbRepository()
+  return new DbAuthentication(accountDbRepository, bcryptAdapter, jwtAdapter, accountDbRepository)
+}
