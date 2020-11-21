a0221eb9ffbfa03eaf888085a756a80cf306938a
Matheus Nunes <mathieusnunes@gmail.com>
2020-11-21 16:00:31 -0300

    feat: add AccountDbRepository add() in the infra layer

--- src/infra/db/account/account-db-repository.ts
+++ src/infra/db/account/account-db-repository.ts
@@ -1,9 +1,19 @@
+import { AddAccountRepository } from '@/data/protocols/db/account/add-account-repository'
 import { LoadAccountByEmailRepository } from '@/data/protocols/db/account/load-account-by-email-repository'
 import { UpdateAccessTokenRepository } from '@/data/protocols/db/account/update-access-token-repository'
 import { AccountModel } from '@/domain/models/account'
+import { AddAccountParams } from '@/domain/usecases/account/add-account'
 import { DbHelper } from '../helpers/db-helper'
 
-export class AccountDbRepository implements LoadAccountByEmailRepository, UpdateAccessTokenRepository {
+export class AccountDbRepository implements LoadAccountByEmailRepository, UpdateAccessTokenRepository, AddAccountRepository {
+  async add (accountData: AddAccountParams): Promise<AccountModel> {
+    const db = await DbHelper.connect()
+    const ids = await db('accounts').insert(accountData)
+    const result = await db('accounts').where({ id: ids[0] }).select()
+    const accountModel = result?.length > 0 ? result[0] : null
+    return accountModel
+  }
+
   async loadByEmail (email: string): Promise<AccountModel> {
     const db = await DbHelper.connect()
     const result = await db('accounts').where({ email }).select()
@@ -16,6 +26,5 @@ export class AccountDbRepository implements LoadAccountByEmailRepository, Update
     await db('accounts').update({
       token
     }).where({ id })
-    return Promise.resolve()
   }
 }
