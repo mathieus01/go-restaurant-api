646e4b3f4d2ba7e984887f4e406ac8bf3e463e37
Matheus Nunes <mathieusnunes@gmail.com>
2021-01-20 19:18:29 -0300

    test: add AddOrderController handle

---
+++ src/presentation/controllers/order/add-order/add-order-controller.spec.ts
@@ -0,0 +1,83 @@
+import { mockAddOrderParams, mockOrderModel } from '@/domain/test/mock-order'
+import { mockValidation } from '@/presentation/test'
+import { HttpRequest, Validation } from '../../authentication/login/login-controller-protocols'
+import { AddOrderController } from './add-order-controller'
+import MockDate from 'mockdate'
+import { badRequest, ok, serverError } from '@/presentation/helpers/http/http-helper'
+import { AddOrder, addOrderParams } from '@/domain/usecases/order/add-order'
+import { OrderModel } from '@/domain/models/order'
+import { throwError } from '@/domain/test/test-helpers'
+
+const mockRequest = (): HttpRequest => ({
+  body: mockAddOrderParams()
+})
+
+const mockAddOrder = (): AddOrder => {
+  class AddOrderStub implements AddOrder {
+    async add (order: addOrderParams): Promise<OrderModel> {
+      return Promise.resolve(mockOrderModel())
+    }
+  }
+  return new AddOrderStub()
+}
+
+interface SutTypes {
+  sut: AddOrderController
+  validationStub: Validation
+  addOrderStub: AddOrder
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = mockValidation()
+  const addOrderStub = mockAddOrder()
+  const sut = new AddOrderController(validationStub, addOrderStub)
+  return {
+    sut,
+    validationStub,
+    addOrderStub
+  }
+}
+
+describe('AddOrderController', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  test('Should call Validate with correct values', async () => {
+    const { sut, validationStub } = makeSut()
+    const validateSpy = jest.spyOn(validationStub, 'validate')
+    await sut.handle(mockRequest())
+    expect(validateSpy).toHaveBeenCalledWith(mockAddOrderParams())
+  })
+
+  test('Should return 400 if Validate returns an erro', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+    const response = await sut.handle(mockRequest())
+    expect(response).toEqual(badRequest(new Error()))
+  })
+
+  test('Should call AddOrder with correct values', async () => {
+    const { sut, addOrderStub } = makeSut()
+    const addOrderSpy = jest.spyOn(addOrderStub, 'add')
+    await sut.handle(mockRequest())
+    expect(addOrderSpy).toHaveBeenCalledWith(mockRequest().body)
+  })
+
+  test('Should return 500 if AddOrder throws', async () => {
+    const { sut, addOrderStub } = makeSut()
+    jest.spyOn(addOrderStub, 'add').mockImplementationOnce(throwError)
+    const response = await sut.handle(mockRequest())
+    expect(response).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 200 on success', async () => {
+    const { sut } = makeSut()
+    const response = await sut.handle(mockRequest())
+    expect(response).toEqual(ok(mockOrderModel()))
+  })
+})
