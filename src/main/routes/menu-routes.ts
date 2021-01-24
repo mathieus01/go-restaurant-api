d59ae6350ca32d5ec4311303df925261d071edf2
Matheus Nunes <mathieusnunes@gmail.com>
2021-01-24 12:00:58 -0300

    feat: add GET list Food route

--- src/main/routes/menu-routes.ts
+++ src/main/routes/menu-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddFoodController } from '../factories/controllers/menu/add-food/add-food-controller-factory'
+import { makeLoadFoodsController } from '../factories/controllers/menu/load-foods/load-foods-controller-factory'
 
 export default (router: Router): void => {
   router.post('/menu', adaptRoute(makeAddFoodController()))
+  router.get('/menu', adaptRoute(makeLoadFoodsController()))
 }
