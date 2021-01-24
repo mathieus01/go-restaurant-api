3d3e070ec357f5f472d591dc48032c16e04cf8e8
Matheus Nunes <mathieusnunes@gmail.com>
2021-01-24 12:01:29 -0300

    test: add GET list Food route

--- src/main/routes/menu-routes.test.ts
+++ src/main/routes/menu-routes.test.ts
@@ -16,10 +16,22 @@ describe('Login Route', () => {
   })
 
   describe('POST /menu', () => {
-    test('Should return 200 on signup', async () => {
+    test('Should return 200 on add Food', async () => {
       await request(app)
         .post('/api/menu')
         .send(mockAddFoodParams())
+        .expect(200)
+    })
+  })
+
+  describe('GET /menu', () => {
+    test('Should return 200 on get list of FOOD', async () => {
+      await Food.create({ food: 'any_food1' })
+      await Food.create({ food: 'any_food2' })
+      await Food.create({ food: 'any_food3' })
+
+      await request(app)
+        .get('/api/menu')
         .expect(200)
     })
   })
