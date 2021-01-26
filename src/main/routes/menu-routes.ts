68cf54ea165f3c34c78a5dfaa84054b729df4c61
Matheus Nunes <mathieusnunes@gmail.com>
2021-01-26 10:01:12 -0300

    feat: add DELETE /food route

--- src/main/routes/menu-routes.ts
+++ src/main/routes/menu-routes.ts
@@ -2,8 +2,10 @@ import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddFoodController } from '../factories/controllers/menu/add-food/add-food-controller-factory'
 import { makeLoadFoodsController } from '../factories/controllers/menu/load-foods/load-foods-controller-factory'
+import { makeRemoveFoodController } from '../factories/controllers/menu/remove-food/remove-food-controller-factory'
 
 export default (router: Router): void => {
   router.post('/menu', adaptRoute(makeAddFoodController()))
   router.get('/menu', adaptRoute(makeLoadFoodsController()))
+  router.delete('/menu/:foodId', adaptRoute(makeRemoveFoodController()))
 }
