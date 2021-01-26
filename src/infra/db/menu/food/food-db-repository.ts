daebe1350306dd27ccedf6fc703c90e31e0f74ec
Matheus Nunes <mathieusnunes@gmail.com>
2021-01-26 08:26:22 -0300

    feat: add FoodDbRepository remove() in infra layer

--- src/infra/db/menu/food/food-db-repository.ts
+++ src/infra/db/menu/food/food-db-repository.ts
@@ -1,11 +1,12 @@
 import { AddFoodRepository } from '@/data/protocols/db/menu/add-food-repository'
 import { LoadFoodByIdRepository } from '@/data/protocols/db/menu/load-food-by-id-repository'
 import { LoadFoodsRepository } from '@/data/protocols/db/menu/load-foods-repository'
+import { RemoveFoodRepository } from '@/data/protocols/db/menu/remove-food-repository'
 import { FoodModel } from '@/domain/models/foods'
 import { AddFoodParams } from '@/domain/usecases/menu/add-food'
 import Food from '@/infra/models/food-model'
 
-export class FoodDbRepository implements AddFoodRepository, LoadFoodByIdRepository, LoadFoodsRepository {
+export class FoodDbRepository implements AddFoodRepository, LoadFoodByIdRepository, LoadFoodsRepository, RemoveFoodRepository {
   async add (addFoodParams: AddFoodParams): Promise<FoodModel> {
     const { food, type, price } = addFoodParams
     const foodModel = await Food.create({ food, price, type_id: type.id })
@@ -20,5 +21,13 @@ export class FoodDbRepository implements AddFoodRepository, LoadFoodByIdReposito
   async loadAllFoods (): Promise<FoodModel[]> {
     const foods = await Food.findAll({ order: [['food', 'ASC']] })
     return foods
+  }
+
+  async remove (foodId: number): Promise<void> {
+    await Food.destroy({
+      where: {
+        id: foodId
+      }
+    })
   }
 }
