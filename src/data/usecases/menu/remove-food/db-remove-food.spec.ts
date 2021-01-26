fb7c75cb684c0c568998a8bd439ea764d31657bc
Matheus Nunes <mathieusnunes@gmail.com>
2021-01-26 08:44:45 -0300

    refactor: refactor DbRemoveFood remove()

--- src/data/usecases/menu/remove-food/db-remove-food.spec.ts
+++ src/data/usecases/menu/remove-food/db-remove-food.spec.ts
@@ -1,50 +1,28 @@
-import { LoadFoodByIdRepository } from '@/data/protocols/db/menu/load-food-by-id-repository'
 import { RemoveFoodRepository } from '@/data/protocols/db/menu/remove-food-repository'
-import { mockLoadFoodByIdRepository, mockRemoveFoodRepository } from '@/data/test/mock-db-food'
+import { mockRemoveFoodRepository } from '@/data/test/mock-db-food'
 import { throwError } from '@/domain/test/test-helpers'
 import { DbRemoveFood } from './db-remove-food'
 
 interface SutTypes {
   sut: DbRemoveFood
-  loadFoodByIdRepositoryStub: LoadFoodByIdRepository
   removeFoodRepositoryStub: RemoveFoodRepository
 }
 
 const makeSut = (): SutTypes => {
-  const loadFoodByIdRepositoryStub = mockLoadFoodByIdRepository()
   const removeFoodRepositoryStub = mockRemoveFoodRepository()
-  const sut = new DbRemoveFood(loadFoodByIdRepositoryStub, removeFoodRepositoryStub)
+  const sut = new DbRemoveFood(removeFoodRepositoryStub)
   return {
     sut,
-    loadFoodByIdRepositoryStub,
     removeFoodRepositoryStub
   }
 }
 
 describe('DbRemoveFood', () => {
-  test('Should call LoadFoodByIdRepository with correct value', async () => {
-    const { sut, loadFoodByIdRepositoryStub } = makeSut()
-    const loadByIdSpy = jest.spyOn(loadFoodByIdRepositoryStub, 'loadById')
-    await sut.remove(1)
-    expect(loadByIdSpy).toHaveBeenLastCalledWith(1)
-  })
-  test('Should throw an error if LoadFoodByIdRepository return null', async () => {
-    const { sut, loadFoodByIdRepositoryStub } = makeSut()
-    jest.spyOn(loadFoodByIdRepositoryStub, 'loadById').mockReturnValueOnce(null)
-    const response = sut.remove(1)
-    await expect(response).rejects.toThrow()
-  })
   test('Should call RemoveFoodRepository with correct value', async () => {
     const { sut, removeFoodRepositoryStub } = makeSut()
     const loadByIdSpy = jest.spyOn(removeFoodRepositoryStub, 'remove')
     await sut.remove(1)
     expect(loadByIdSpy).toHaveBeenLastCalledWith(1)
-  })
-  test('Should throw if LoadFoodByIdRepository throws', async () => {
-    const { sut, loadFoodByIdRepositoryStub } = makeSut()
-    jest.spyOn(loadFoodByIdRepositoryStub, 'loadById').mockImplementationOnce(throwError)
-    const response = sut.remove(1)
-    await expect(response).rejects.toThrow()
   })
   test('Should throw if RemoveFoodRepository throws', async () => {
     const { sut, removeFoodRepositoryStub } = makeSut()
--- src/data/usecases/menu/remove-food/db-remove-food.ts
+++ src/data/usecases/menu/remove-food/db-remove-food.ts
@@ -1,17 +1,11 @@
-import { LoadFoodByIdRepository } from '@/data/protocols/db/menu/load-food-by-id-repository'
 import { RemoveFoodRepository } from '@/data/protocols/db/menu/remove-food-repository'
 import { RemoveFood } from '@/domain/usecases/menu/remove-food'
 
 export class DbRemoveFood implements RemoveFood {
   constructor (
-    private readonly loadFoodByIdRepository: LoadFoodByIdRepository,
     private readonly removeFoodRepository: RemoveFoodRepository) {}
 
   async remove (foodId: number): Promise<void> {
-    const food = await this.loadFoodByIdRepository.loadById(foodId)
-    if (!food) {
-      throw new Error('Food not found')
-    }
     await this.removeFoodRepository.remove(foodId)
   }
 }
