34ab59f3d195f38b26b5cd60f9756324322b7071
Matheus Nunes <mathieusnunes@gmail.com>
2021-01-27 09:37:44 -0300

    feat: add GET /orders/user/:userId route

--- src/main/routes/order-routes.ts
+++ src/main/routes/order-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { makeAddOrderController } from '@/main/factories/controllers/order/add-order/add-order-controller-factory'
 import { adaptRoute } from '../adapters/express-route-adapter'
+import { makeLoadOrdersByUserController } from '../factories/controllers/order/load-orders-by-user/load-orders-by-user-controller-factory'
 
 export default (router: Router): void => {
-  router.post('/order', adaptRoute(makeAddOrderController()))
+  router.post('/orders', adaptRoute(makeAddOrderController()))
+  router.post('/orders/users/:userId', adaptRoute(makeLoadOrdersByUserController()))
 }
