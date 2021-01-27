79cec1e5ada363d8fc78951a4b2ae5481b9dfae4
Matheus Nunes <mathieusnunes@gmail.com>
2021-01-27 09:10:44 -0300

    test: add LoadOrdersByUserController handle in the presentation layer

---
+++ src/presentation/controllers/order/load-orders-by-user/load-orders-by-user-controller.spec.ts
@@ -0,0 +1,67 @@
+import { OrderModel } from '@/domain/models/order'
+import { mockOrderModel } from '@/domain/test/mock-order'
+import { throwError } from '@/domain/test/test-helpers'
+import { LoadOrdersByUser } from '@/domain/usecases/order/load-orders-by-user'
+import { ok, serverError } from '@/presentation/helpers/http/http-helper'
+import { HttpRequest } from '../../authentication/login/login-controller-protocols'
+import { LoadOrdersByUserController } from './load-orders-by-user-controller'
+import MockDate from 'mockdate'
+
+const mockRequest = (): HttpRequest => ({
+  params: {
+    userId: 1
+  }
+})
+
+const mockLoadOrdersByUser = (): LoadOrdersByUser => {
+  class LoadOrdersByUserStub implements LoadOrdersByUser {
+    async loadOrdersByUser (userId: number): Promise<OrderModel[]> {
+      return Promise.resolve([mockOrderModel()])
+    }
+  }
+  return new LoadOrdersByUserStub()
+}
+
+interface SutTypes {
+  sut: LoadOrdersByUserController
+  loadOrdersByUserStub: LoadOrdersByUser
+}
+
+const makeSut = (): SutTypes => {
+  const loadOrdersByUserStub = mockLoadOrdersByUser()
+  const sut = new LoadOrdersByUserController(loadOrdersByUserStub)
+  return {
+    sut,
+    loadOrdersByUserStub
+  }
+}
+
+describe('LoadOrdersByUserController', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  test('Should call loadOrdersByUser with correct values', async () => {
+    const { sut, loadOrdersByUserStub } = makeSut()
+    const loadOrdersByUserSpy = jest.spyOn(loadOrdersByUserStub, 'loadOrdersByUser')
+    await sut.handle(mockRequest())
+    expect(loadOrdersByUserSpy).toHaveBeenCalledWith(mockRequest().body)
+  })
+
+  test('Should return 500 if loadOrdersByUser throws', async () => {
+    const { sut, loadOrdersByUserStub } = makeSut()
+    jest.spyOn(loadOrdersByUserStub, 'loadOrdersByUser').mockImplementationOnce(throwError)
+    const response = await sut.handle(mockRequest())
+    expect(response).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 200 on success', async () => {
+    const { sut } = makeSut()
+    const response = await sut.handle(mockRequest())
+    expect(response).toEqual(ok([mockOrderModel()]))
+  })
+})
