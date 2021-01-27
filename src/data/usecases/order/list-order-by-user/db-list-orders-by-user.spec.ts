71855dd79d50a626b25937e35008a6859dcddbf4
Matheus Nunes <mathieusnunes@gmail.com>
2021-01-27 07:47:37 -0300

    test: add DbListOrdersByUser listOrdersByUser in data layer

---
+++ src/data/usecases/order/list-order-by-user/db-list-orders-by-user.spec.ts
@@ -0,0 +1,57 @@
+import { ListOrdersByUserRepository } from '@/data/protocols/db/order/list-orders-by-user-repository'
+import { OrderModel } from '@/domain/models/order'
+import { mockOrderModel } from '@/domain/test/mock-order'
+import { throwError } from '@/domain/test/test-helpers'
+import { DbListOrdersByUser } from './db-list-orders-by-user'
+import MockDate from 'mockdate'
+
+const mockListOrdersByUser = (): ListOrdersByUserRepository => {
+  class ListOrdersByUserStub implements ListOrdersByUserRepository {
+    async listOrdersByUser (userId: number): Promise<OrderModel[]> {
+      return Promise.resolve([mockOrderModel()])
+    }
+  }
+  return new ListOrdersByUserStub()
+}
+
+interface SutTypes {
+  sut: DbListOrdersByUser
+  listOrdersByUserRepositoryStub: ListOrdersByUserRepository
+}
+
+const makeSut = (): SutTypes => {
+  const listOrdersByUserRepositoryStub = mockListOrdersByUser()
+  const sut = new DbListOrdersByUser(listOrdersByUserRepositoryStub)
+  return {
+    sut,
+    listOrdersByUserRepositoryStub
+  }
+}
+
+describe('DbListOrdersByUser', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  test('Should call ListOrdersByUserRepository with correct values', async () => {
+    const { sut, listOrdersByUserRepositoryStub } = makeSut()
+    const listOrdersByUserSpy = jest.spyOn(listOrdersByUserRepositoryStub, 'listOrdersByUser')
+    await sut.listOrdersByUser(1)
+    expect(listOrdersByUserSpy).toHaveBeenLastCalledWith(1)
+  })
+  test('Should throw if ListOrdersByUserRepository throws', async () => {
+    const { sut, listOrdersByUserRepositoryStub } = makeSut()
+    jest.spyOn(listOrdersByUserRepositoryStub, 'listOrdersByUser').mockImplementationOnce(throwError)
+    const response = sut.listOrdersByUser(1)
+    await expect(response).rejects.toThrow()
+  })
+  test('Should return list of orders on success', async () => {
+    const { sut } = makeSut()
+    const orders = await sut.listOrdersByUser(1)
+    expect(orders).toEqual([mockOrderModel()])
+  })
+})
