4064709154790326a4d36a3cd1ff09b4a4d22ddb
Matheus Nunes <mathieusnunes@gmail.com>
2021-03-02 18:09:16 -0300

    feat: add GET /orders/:orderId route

--- src/main/routes/order-routes.ts
+++ src/main/routes/order-routes.ts
@@ -1,9 +1,10 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeAddOrderController, makeLoadOrdersByUserController, makeUpdateOrderStatusController } from '@/main/factories/controllers'
+import { makeAddOrderController, makeLoadOrderByIdController, makeLoadOrdersByUserController, makeUpdateOrderStatusController } from '@/main/factories/controllers'
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/orders', adaptRoute(makeAddOrderController()))
   router.post('/orders/users/:userId', adaptRoute(makeLoadOrdersByUserController()))
   router.put('/orders/:orderId/status', adaptRoute(makeUpdateOrderStatusController()))
+  router.get('/orders/:orderId', adaptRoute(makeLoadOrderByIdController()))
 }
