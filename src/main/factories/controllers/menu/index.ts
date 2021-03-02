214bec6bdff28e370305a17f652bdc7122556c90
Matheus Nunes <mathieusnunes@gmail.com>
2021-03-02 17:32:30 -0300

    feat: add GET /menu/:foodId route

--- src/main/factories/controllers/menu/index.ts
+++ src/main/factories/controllers/menu/index.ts
@@ -1,3 +1,4 @@
 export * from './add-food/add-food-controller-factory'
 export * from './load-foods-by-restaurant/load-foods-by-restaurant-controller-factory'
 export * from './remove-food/remove-food-controller-factory'
+export * from './load-food-by-id/load-food-by-id-controller-factory'
--- src/main/routes/menu-routes.ts
+++ src/main/routes/menu-routes.ts
@@ -1,9 +1,10 @@
-import { makeAddFoodController, makeLoadFoodsByRestaurantController, makeRemoveFoodController } from '@/main/factories/controllers'
+import { makeAddFoodController, makeLoadFoodByIdController, makeLoadFoodsByRestaurantController, makeRemoveFoodController } from '@/main/factories/controllers'
 import { adaptRoute } from '@/main/adapters'
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/restaurants/:restaurantId/menu', adaptRoute(makeAddFoodController()))
   router.get('/restaurants/:restaurantId/menu', adaptRoute(makeLoadFoodsByRestaurantController()))
+  router.get('/menu/:foodId', adaptRoute(makeLoadFoodByIdController()))
   router.delete('/menu/:foodId', adaptRoute(makeRemoveFoodController()))
 }
