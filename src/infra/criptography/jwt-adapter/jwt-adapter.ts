0cdcc01b6c0d440761127820d550f778e71e43c1
Matheus Nunes <mathieusnunes@gmail.com>
2021-03-14 11:51:29 -0300

    feat: add decrypt to JwtAdapter in infra layer

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -1,11 +1,16 @@
-import { Encrypter } from '@/data/protocols/cryptography'
+import { Decrypter, Encrypter } from '@/data/protocols/cryptography'
 import jwt from 'jsonwebtoken'
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor (private readonly secret: string) {}
 
   async encrypt (id: number): Promise<string> {
     const accessToken = await jwt.sign({ id }, this.secret)
     return accessToken
+  }
+
+  async decrypt (token: string): Promise<string> {
+    const value: any = await jwt.verify(token, this.secret)
+    return value
   }
 }
